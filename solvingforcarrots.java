2fb914a079b9e170d79b6c800e74d0ba2e7408e9
Pradeep Siddhartha Singh Bisen <pscrusher101@gmail.com>
2020-03-13 02:09:23 +0100

    Add files via upload

---
+++ solvingforcarrots.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class solvingforcarrots {
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+	    obj.nextInt();
+	    int carrotsgiven = obj.nextInt();
+
+	    System.out.println(carrotsgiven);
+	   }
+
+}
---
+++ stuckinatimeloop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class stuckinatimeloop {
+	public static void main(String[] args){
+		
+		Scanner obj = new Scanner(System.in);
+		int N = obj.nextInt();
+		
+		for(int i = 1; i<=N;i++){
+			System.out.println(i + " " + "Abracadabra");
+		}
+	}
+	
+
+}
